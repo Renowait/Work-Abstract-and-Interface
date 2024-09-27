9099b033ab479b710b25254b67b703d496b6b784
อาบัตตาคัมมมม <125907972+Renowait@users.noreply.github.com>
2024-09-27 12:35:49 +0700

    Add files via upload

---
+++ homework.dart
@@ -0,0 +1,48 @@
+//นายนธีพัฒน์ เถื่อนกวา ปสสท3 1147
+import 'dart:math';
+
+abstract class  Shape{
+  calculateArea();
+}
+abstract class Drawable implements Shape {
+  draw();
+}
+
+class Circle extends Shape implements Drawable {
+  int? radius;
+  Circle(this.radius);
+  @override
+
+   calculateArea() {
+    return pi * pow(radius!, 2);
+  }
+  @override
+  draw() {
+    print("Drawing Circle with radius:${radius}");
+  }
+}
+
+class Rectangle extends Shape implements Drawable{
+  int? width;
+  int? heigth;
+
+  Rectangle(this.heigth,this.width);
+
+  @override
+  calculateArea() {
+    // TODO: implement calculateArea
+    return print('Area of Rectangle: ${width! * heigth!}');
+  }
+  draw() => print("Drawing Rectangle with width: ${width} and height: ${heigth}");
+}
+void main(List<String> args) {
+  var cir =Circle(5);
+  var rec = Rectangle(10, 20);
+
+  cir.draw();
+  print('Area of Circle: ${cir.calculateArea().toStringAsFixed(2)}');
+
+  rec.draw();
+  rec.calculateArea();
+
+}
